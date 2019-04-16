11a560cf0eb2606cb4c0919f0e3eae883a463ad4
isigalvarez <42265458+isigalvarez@users.noreply.github.com>
2019-04-16 17:36:18 +0200

    Added few code lines before commit and push

--- extract_nc.py
+++ extract_nc.py
@@ -6,10 +6,22 @@
 # ===========================================================
 
 import os
-import glob
 
 # == Define parameters ======================================
-# path to some grid files
+# Path to some grid files
 dataPath = 'testData/test1_DoesThisWork/output/'
 # ===========================================================
+
+# == Classifying data =======================================
+# First we extract all the files inside output dir
+files_all = os.listdir(dataPath)
+# Separate them in 'conc', 'time' and 'partpos'
+files_conc = [f for f in files_all if f.startswith('grid_conc') == True]
+files_time = [f for f in files_all if f.startswith('grid_time') == True]
+files_ppos = [f for f in files_all if f.startswith('partposit') == True]
+# ===========================================================
+
+# == Trying to extract info =================================
+
+
 
