c270da3b6c21368da9315a8605a2d30dab1aa8b2
isigalvarez <42265458+isigalvarez@users.noreply.github.com>
2019-04-17 17:01:44 +0200

    Added a function to extract clustered trajectories data

--- explore_traj.py
+++ explore_traj.py
@@ -1,12 +1,41 @@
 # !usr/bin/env python3
 # ===========================================================
 # Created on 17/04/2019
-# This script is a first aproximation to extract flexpart
-# trajectories output using python
+# This script is a first aproximation to extract flexpart cluster
+# trajectories from the file 'trajectories.txt'
+#
+# The file 'trajectories.txt' contains a short header with
+# information about the release definition. After that, there
+# are 41 columns with detailed information about the clustered
+# trajectories and one row per timestep of the simulation
+#
+# This variables and its names are, in order:
+# j             Release point
+# t             Time elapsed (s) since the middle of the release
+# xcenter       Longitude of the release plume
+# ycenter       Latitude of the release plume
+# zcenter       Height of the release plume
+# topocenter    Mean topography underlying all particles
+# hmixcenter    Mean mixing height for all particles
+# tropocenter   Mean tropopause height at the positions of particles
+# pvcenter      Mean PV for all particles
+# rmsdist       Total horizontal rms distance before clustering
+# rms           Total horizontal rms distance after clustering
+# zrmsdist      Total vertical rms distance before clustering
+# zrms          Total vertical rms distance after clustering
+# hmixfract     Fraction of particles under the mean mixing height
+# pvfract       Fraction of particles with PV<2pvu
+# tropofract    Fraction of particles within the troposphere
+# xclust_k      Longitude of the k-th cluster
+# yclust_k      Latitude of the k-th cluster
+# zclust_k      Height of the k-th cluster
+# fclust_k      Fraction of particles belonging k-th cluster
+# rmsclust_k    Horizontal rms distance for k-th cluster
 # ===========================================================
 
 import os
 import numpy as np
+import pandas as pd
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
 
@@ -14,17 +43,37 @@ from matplotlib.backends.backend_pdf import PdfPages
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 
 # == Extract Data ===========================================
+
+
 def extract_trajectories(filePath):
     '''
-    This function extract the trajectories data from a txt file. 
+    This function extract the trajectories data from a txt file
+    and saves it to a pandas Dataframe.
     '''
-
+    # Define the variables names
+    names = ['j', 't', 'xcenter', 'ycenter', 'zcenter', 'topocenter',
+             'hmixcenter', 'tropocenter', 'pvcenter', 'rmsdist',
+             'rms', 'zrmsdist', 'zrms', 'hmixfract', 'pvfract',
+             'tropofract']
+    names_cluster = ['xclust', 'yclust', 'zclust', 'fclust',
+                     'rmsclust']
+    # Define the number of clusters
+    nClusters = 5
+    # Build the whole namelist
+    for i in range(nClusters):
+        names += [s+f'_{i+1}' for s in names_cluster]
+    # Extract the data
+    df = pd.read_csv(filePath, engine='python', sep='\s+',
+                     skiprows=5, header=None, names=names)
+    # Return the data
+    return df
 # ===========================================================
+
 
 if __name__ == '__main__':
     # == Define parameters ==================================
     # Path to some grid files
-    dataPath = 'testData/output_05_BwdTraj_SegunManual_netCDF/'
+    dataPath = 'testData/output_03_MassPlumeTrajectories_netCDF/'
     # =======================================================
 
     # == Find data files ====================================
@@ -36,3 +85,5 @@ if __name__ == '__main__':
     filePath = dataPath + files[0]
     # =======================================================
 
+    # == Extract data =======================================
+    df = extract_trajectories(filePath)
