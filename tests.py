b689e8fdc7dbb10ccf5b161f75e9869e24b9c3be
Isidoro <isigalvarez@gmail.com>
2019-04-23 17:28:52 +0200

    minor changes

--- tests.py
+++ tests.py
@@ -12,17 +12,17 @@ from FLEXPARTRun import *
 dirPath = '/home/isi/FLEXPART/flexpart10_git/Runs/FPRun_02/'
 flexpartPath = '/home/isi/FLEXPART/flexpart10_git/'
 meteoPath = '/home/isi/FLEXPART/Meteo/ECMWF/20170829_EA'
-params = [  {   'IDATE1': 20170831, 'ITIME1': 105900,
+params = [  { 'IDATE1': 20170831, 'ITIME1': 105900,
                 'IDATE2': 20170831, 'ITIME2': 110000,
                 'LON1': -22.981, 'LON2': -22.952,
                 'LAT1': 16.786, 'LAT2': 16.787,
                 'Z1': 833.51, 'Z2': 833.52,
                 'ZKIND': 1, 'MASS': 100000000.0,
                 'PARTS': 1000, 'COMMENT': 'RELEASE 1'},
-            {   'IDATE1': 20170831, 'ITIME1': 115900,
-                'IDATE2': 20170831, 'ITIME2': 120000,
-                'LON1': -23.981, 'LON2': -23.952,
-                'LAT1': 17.786, 'LAT2': 17.787,
+            {   'IDATE1': 20170831, 'ITIME1': 105900,
+                'IDATE2': 20170831, 'ITIME2': 110000,
+                'LON1': -22.981, 'LON2': -22.952,
+                'LAT1': 16.786, 'LAT2': 16.787,
                 'Z1': 833.51, 'Z2': 833.52,
                 'ZKIND': 1, 'MASS': 100000000.0,
                 'PARTS': 1000, 'COMMENT': 'RELEASE 1'}]
@@ -32,3 +32,14 @@ FPRun.prepareFiles()
 FPRun.write_COMMAND()
 FPRun.write_OUTGRID()
 FPRun.write_RELEASES(params)
+
+# Create a copy of the existing RELEASES filename
+shutil.copy(dirPath+'options/RELEASES',dirPath+'options/RELEASES.original')
+# Open a namelsit with f90nml
+nml = f90nml.read(dirPath+'options/RELEASES.original')
+# Change something
+for key in command.keys():
+    nml['releases'][key] = command[key]
+# Write the change
+nml.write(dirPath+'options/RELEASES_temp')
+os.replace(dirPath+'options/RELEASES_temp',dirPath+'options/RELEASES')
